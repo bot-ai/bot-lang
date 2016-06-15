e42796c57f9656995f64285ebcdb679718ced255
Rob Ellis <rellis@slack-corp.com>
2016-06-15 09:15:56 -0700

    adds a testAll method

--- test/basic.js
+++ test/basic.js
@@ -5,3 +5,6 @@ var assert = require("assert");
 assert(kw.testInput("yes", "I am sure"));
 assert(kw.testInput("yes", "Nope") === false);
 assert(kw.testInput("no", "Nope"));
+// console.log(kw.testInput("help", "gee thanks"));
+
+assert.deepEqual(kw.testAll("eww , shut up , I have to go"), [ 'disgust', 'goodbye', 'stop' ]);
