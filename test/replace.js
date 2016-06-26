fac34e2fa83e6a326d5e8c756841d46cb430ddb5
Rob Ellis <rellis@slack-corp.com>
2016-06-25 21:42:39 -0700

    Changed data around and added contractions

---
+++ test/replace.js
@@ -0,0 +1,4 @@
+var replace = require("../lib/replace");
+
+console.log(replace.contraction("no you haven't"));
+console.log(replace.contraction("you'll laugh. you'r or you're or your"));
--- test/tag.js
+++ test/tag.js
@@ -1,10 +1,11 @@
 // TODO Add a real suite.
-var kw = require("../lib/keywords");
+var kw = require("../lib/tag");
 var assert = require("assert");
 
 assert(kw.testInput("yes", "I am sure"));
 assert(kw.testInput("yes", "Nope") === false);
 assert(kw.testInput("no", "Nope"));
-// console.log(kw.testInput("help", "gee thanks"));
 
 assert.deepEqual(kw.testAll("eww , shut up , I have to go"), [ 'disgust', 'goodbye', 'stop' ]);
+
+console.log(kw.testAll("eww , shut up , I have to go"));
